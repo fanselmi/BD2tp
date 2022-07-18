058c89cb762d29344879d1492cc0e979d43710ee
sferraris <sferraris@itba.edu.ar>
2022-07-18 18:55:19 -0300

    saco el username del login

--- frontend/src/app/views/pages/login/login.component.ts
+++ frontend/src/app/views/pages/login/login.component.ts
@@ -20,7 +20,6 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.email, Validators.required]),
-      username: new FormControl('', [Validators.maxLength(150), Validators.required]),
       password: new FormControl('', [Validators.required])
     })
   }
@@ -29,17 +28,13 @@ export class LoginComponent implements OnInit {
     return this.loginForm.get('email')
   }
 
-  get username(){
-    return this.loginForm.get('username')
-  }
-
   get password(){
     return this.loginForm.get('password')
   }
 
   async onSubmit(): Promise<void> {
     this.loading = true;
-    this.userService.login(new UserModel(this.username?.value, this.email?.value, this.password?.value))
+    this.userService.login(new UserModel('',this.email?.value, this.password?.value))
       .pipe(
         first(),
         catchError(err => {
